938ef93cc24c2000def9360e1cd2678e37a2026d
ihateyouall-dev <ihateyouallfuckyou@proton.me>
2026-04-19 18:46:01 +0300

    tests(parsers): add tests for date and time parsers

--- tests/parsers/parsers_tests.cpp
+++ tests/parsers/parsers_tests.cpp
@@ -1,19 +1,29 @@
-#include "lines/temporal/timepoint.hpp"
 #include <client-utils/parsers.hpp>
 #include <gtest/gtest.h>
 
-TEST(Parsers, Timepoint) {
+TEST(Parsers, Date) {
+    EXPECT_EQ(parse_date("1970.01.01"), Lines::Temporal::Date{Lines::Temporal::Days{0}});
+    EXPECT_EQ(parse_date("1969.12.31"), Lines::Temporal::Date{Lines::Temporal::Days{-1}});
+
+    EXPECT_THROW(parse_date("1970.13.01"), std::out_of_range);
+    EXPECT_THROW(parse_date("1970.01.32"), std::out_of_range);
+}
+
+TEST(Parsers, Time) {
+    EXPECT_EQ(parse_time("00:00:00"), Lines::Temporal::Timestamp{Lines::Temporal::Seconds{0}});
+    EXPECT_EQ(parse_time("23:59:59"), Lines::Temporal::Timestamp{Lines::Temporal::Seconds{86399}});
+
+    EXPECT_THROW(parse_time("24:00:00"), std::out_of_range);
+    EXPECT_THROW(parse_time("00:60:00"), std::out_of_range);
+    EXPECT_THROW(parse_time("00:00:60"), std::out_of_range);
+
+}
+
+TEST(Parsers, TimePoint) {
     EXPECT_EQ(parse_timepoint("1970.01.01"),
               Lines::Temporal::TimePoint{Lines::Temporal::Seconds{0}});
     EXPECT_EQ(parse_timepoint("1970.01.01_23:59:59"),
               Lines::Temporal::TimePoint{Lines::Temporal::Seconds{86399}});
-    EXPECT_EQ(parse_timepoint("1969.12.31"), Lines::Temporal::TimePoint{Lines::Temporal::Days{-1}});
-
-    EXPECT_THROW(parse_timepoint("1970.13.01"), std::out_of_range);
-    EXPECT_THROW(parse_timepoint("1970.01.32"), std::out_of_range);
-    EXPECT_THROW(parse_timepoint("1970.01.01_24:00:00"), std::out_of_range);
-    EXPECT_THROW(parse_timepoint("1970.01.01_00:60:00"), std::out_of_range);
-    EXPECT_THROW(parse_timepoint("1970.01.01_00:00:60"), std::out_of_range);
 
     EXPECT_THROW(parse_timepoint("Garbage"), std::invalid_argument);
 }
