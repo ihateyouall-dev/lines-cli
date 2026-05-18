e5dbb3100aa703a5f1e19fefaea68e10dfa59249
ihateyouall-dev <ihateyouallfuckyou@proton.me>
2026-05-18 22:32:50 +0300

    refactor: add version.h

--- src/cli/root.cpp
+++ src/cli/root.cpp
@@ -1,11 +1,11 @@
 #include "cli/root.hpp"
 
 #include "cli/docs/docs.hpp"
-#include "lines/version.h"
+#include "version.h"
 
 Lines::CLI::Root::Root() {
     add_flag_callback("-v,--version",
-                      []() -> void { std::cout << "Lines CLI " << LINES_VERSION << '\n'; });
+                      []() -> void { std::cout << "Lines CLI " << LINES_CLI_VERSION_STR << '\n'; });
     _tasks.init(*this);
     _docs.init(*this);
     this->callback([this]() -> void {
